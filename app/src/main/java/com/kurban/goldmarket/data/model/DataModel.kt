c35f4cb20d9bd881941d4af868cdef8b875ee674
RecepK <kurbanrecep@gmail.com>
2021-09-23 22:21:47 +0300

    Added remote data source.

--- app/src/main/java/com/kurban/goldmarket/data/model/DataModel.kt
+++ app/src/main/java/com/kurban/goldmarket/data/model/DataModel.kt
@@ -1,2 +1,9 @@
 package com.kurban.goldmarket.data.model
 
+
+data class DataModel(
+    val name: String,
+    val buy: String,
+    val sell: String,
+    val time: String,
+)
--- app/src/main/java/com/kurban/goldmarket/data/remote/Api.kt
+++ app/src/main/java/com/kurban/goldmarket/data/remote/Api.kt
@@ -1,2 +1,8 @@
 package com.kurban.goldmarket.data.remote
 
+import com.kurban.goldmarket.data.model.DataModel
+
+interface Api {
+
+    fun getList(): List<DataModel>
+}
