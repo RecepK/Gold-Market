21845dfa932cd227029ce880644466fb1034ef7e
RecepK <kurbanrecep@gmail.com>
2021-09-23 22:50:03 +0300

    Added MainViewState

--- app/src/main/java/com/kurban/goldmarket/presentation/MainViewModel.kt
+++ app/src/main/java/com/kurban/goldmarket/presentation/MainViewModel.kt
@@ -4,11 +4,16 @@ import androidx.lifecycle.ViewModel
 import com.kurban.goldmarket.domain.usecase.UseCase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 
 class MainViewModel(private val useCase: UseCase) : ViewModel() {
+
+    private val _state: MutableStateFlow<MainViewState> = MutableStateFlow(MainViewState.Loading)
+    val state: StateFlow<MainViewState> = _state
 
     init {
         getGoldList()
@@ -17,7 +22,13 @@ class MainViewModel(private val useCase: UseCase) : ViewModel() {
     private fun getGoldList() {
         CoroutineScope(Dispatchers.IO).launch {
             useCase.getGoldList().collect {
-                    list ->
+                val list = it.data
+
+                list?.let {
+                    _state.emit(MainViewState.Success(value = list))
+                } ?: run {
+                    _state.emit(MainViewState.Error(error = "MainViewModel EmptyList"))
+                }
             }
         }
     }
---
+++ app/src/main/java/com/kurban/goldmarket/presentation/MainViewState.kt
@@ -0,0 +1,10 @@
+package com.kurban.goldmarket.presentation
+
+import com.kurban.goldmarket.domain.model.Model
+
+
+sealed class MainViewState(val data: List<Model>? = null, val message: String? = null) {
+    object Loading : MainViewState()
+    data class Success(val value: List<Model>) : MainViewState(data = value)
+    data class Error(val error: String) : MainViewState(message = error)
+}
