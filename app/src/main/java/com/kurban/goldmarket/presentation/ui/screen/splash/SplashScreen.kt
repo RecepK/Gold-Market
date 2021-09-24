d85d293470358391056333b2df80f78b38d17f43
RecepK <kurbanrecep@gmail.com>
2021-09-24 21:45:10 +0300

    The package was created for SplashScreen.

---
+++ app/src/main/java/com/kurban/goldmarket/presentation/ui/screen/splash/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.kurban.goldmarket.presentation.ui.screen.splash
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun SplashScreen() {
+
+    Text(text = "SplashScreen")
+}
